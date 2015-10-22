791a2ca82b007cb2ba31d5c11228abd71c451d39
Joshua Flores Tuz <josshft@gmail.com>
2015-10-22 12:08:12 -0500

    implementacion brujula

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -67,6 +67,27 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
+        "id": "cordova-plugin-device-orientation.CompassError",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassHeading.js",
+        "id": "cordova-plugin-device-orientation.CompassHeading",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/compass.js",
+        "id": "cordova-plugin-device-orientation.compass",
+        "clobbers": [
+            "navigator.compass"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -76,7 +97,8 @@ module.exports.metadata =
     "cordova-plugin-device": "1.0.1",
     "cordova-plugin-device-motion": "1.1.1",
     "cordova-plugin-dialogs": "1.1.1",
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-device-orientation": "1.0.1"
 }
 // BOTTOM OF METADATA
 });
---
+++ www/js/compass.js
@@ -0,0 +1,69 @@
+/**
+ * Created by Joshua on 21/10/2015.
+ */
+var gWatchID = null;
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    startWatch();
+}
+
+function startWatch() {
+    var options = { frequency: 1000 };
+    if (!gWatchID) {
+        gWatchID = navigator.compass.watchHeading(onSuccess, onError, options);
+    }
+}
+
+function stopWatch() {
+    if (gWatchID) {
+        navigator.compass.clearWatch(watchID);
+        gWatchID = null;
+    }
+}
+
+function onSuccess(heading) {
+    var element = document.getElementById('heading');
+    element.innerHTML = 'Heading: ' + heading.magneticHeading + '<br>';
+
+    var direction = heading.magneticHeading;
+
+    var img = document.getElementById('image');
+    img.style.margin = 'auto';
+    img.style.transformOrigin = '50% 50%';
+    img.style.webkitTransformOrigin = '50% 50%';
+    img.style.msTransformOrigin = '50% 50%';
+
+    img.style.webkitTransform = 'rotate('+(-direction)+'deg)';
+    img.style.mozTransform    = 'rotate('+(-direction)+'deg)';
+    img.style.msTransform     = 'rotate('+(-direction)+'deg)';
+    img.style.oTransform      = 'rotate('+(-direction)+'deg)';
+    img.style.transform       = 'rotate('+(-direction)+'deg)';
+
+    if (direction>0 && direction<90){
+        element.innerHTML = 'Direction: Noreste (NE)';
+    }
+    if (direction == 90){
+        element.innerHTML = 'Direction: Este (E)';
+    }
+    if (direction>90 && direction<180){
+        element.innerHTML = 'Direction: Sureste (SE)';
+    }
+    if (direction == 180){
+        element.innerHTML = 'Direction: Sur (S)';
+    }
+    if (direction>180 && direction<270){
+        element.innerHTML = 'Direction: Suroeste (SO)';
+    }
+    if (direction == 270){
+        element.innerHTML = 'Direction: Oeste (O)';
+    }
+    if (direction>270 && direction<359){
+        element.innerHTML = 'Direction: Noroeste (NO)';
+    }
+    if (direction == 359){
+        element.innerHTML = 'Direction: Norte (N)';
+    }
+}
+function onError(compassError) {
+    alert('Compass error: ' + compassError.code);
+}
