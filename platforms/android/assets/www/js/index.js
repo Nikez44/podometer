40c60768e24698ed48feb09adf2904822c4affe4
Nikez44 <nike-o_94@hotmail.com>
2015-10-21 19:01:28 -0500

    Podometro listo*

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -4,6 +4,8 @@
 var watchID = null;
 
 var steps = 0;
+var calories = 0;
+var distance = 0;
 
 var prevX = 0;
 var prevY = 0;
@@ -15,79 +17,60 @@ var currZ = 0;
 
 var moveX = false;
 var moveY = false;
+var isNoise = true;
 
-var ALPHA = 2;
+var NOISE = 2;
 
 function startWatch(){
-    var options = { frequency: 500 };
+    steps = 0;
+    calories = 0;
+    distance = 0;
+    var options = { frequency: 1000 };
     watchID = navigator.accelerometer.watchAcceleration(onSucess, onError, options);
 }
 
-function stopWatch(){
-    if(watchID){
-        navigator.accelerometer.clearWatch(watchID);
-        watchID = null;
-    }
-}
-
 function onSucess(acceleration){
-
-    $('#steps').html(steps);
 
     prevX = currX;
     prevY = currY;
     prevZ = currZ;
 
-    $('#previous').html(' Aceleracion X:' + prevX + '<br/>' +
-        ' Aceleracion Y:' + prevY + '<br/>' +
-        ' Aceleracion Z:' + prevZ + '<br/>');
-
     currX = acceleration.x;
     currY = acceleration.y;
     currZ = acceleration.z;
 
-    $('#current').html(' Aceleracion X:' + currX + '<br/>' +
-        ' Aceleracion Y:' + currY + '<br/>' +
-        ' Aceleracion Z:' + currZ + '<br/>');
-
     difX = Math.abs(currX - prevX);
     difY = Math.abs(currY - prevY);
-    difZ = Math.abs(currZ - prevZ);
 
-    $('#difX').html('Dif x: '+ difX);
-    $('#difY').html('Dif y: '+ difY);
-    $('#difZ').html('Dif z: '+ difZ);
-
-    /*
-
-    if (currX != (prevX + NOISE) || currX != (prevX - NOISE)){
-        moveX = true;
+    if (Math.abs(currX) > NOISE && Math.abs(currY) > NOISE){
+        isNoise = false;
+    }else{
+        isNoise = true;
     }
 
-    if (currY != (prevY + NOISE) || currY != (prevY - NOISE)){
-        moveY = true;
-    }
-
-    if (difX > ALPHA && difY > ALPHA){
-        steps++;
-    }*/
-
-    if (difX > ALPHA && difX < ALPHA*2){
+    if (difX > 1.5){
         moveX = true;
     }else{
         moveX =  false
     }
 
-    if (difY >  ALPHA && difY < ALPHA*2){
+    if (difY > 1 ){
         moveY = true;
     }else{
         moveY = false
     }
 
-    if (moveX && moveY){
+    if (moveX && moveY && !isNoise){
         steps++;
+        //Suponiendo que le persona pesa aproximadamente 72 kg y lleva una velocidad de 4 km/h
+        calories += 0.039;
+        //Tomando del largo de paso promedio segun La Universidad Estadounidense de la Medicina Deportiva
+        distance += 0.8;
     }
 
+    $('#steps').html(steps);
+    $('#calories').html(calories.toFixed(2));
+    $('#distance').html(distance.toFixed(2));
 }
 
 function onError(){
